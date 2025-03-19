7da6dafcf56d7c7d39904c0a395646e74bb8da6e
theophilhenry <theophil730730@gmail.com>
2025-03-19 14:15:41 +0700

    fix

---
+++ hiragana-and-katakana.py
@@ -0,0 +1,132 @@
+import random
+import os
+import time
+
+
+def clear(): return os.system('cls' if os.name == 'nt' else 'clear')
+
+
+# Glosasary of all japanese hiragana
+glossary_hiragana = {
+    'あ': 'a', 'い': 'i', 'う': 'u', 'え': 'e', 'お': 'o',
+    'か': 'ka', 'き': 'ki', 'く': 'ku', 'け': 'ke', 'こ': 'ko',
+    'さ': 'sa', 'し': 'shi', 'す': 'su', 'せ': 'se', 'そ': 'so',
+    'た': 'ta', 'ち': 'chi', 'つ': 'tsu', 'て': 'te', 'と': 'to',
+    'な': 'na', 'に': 'ni', 'ぬ': 'nu', 'ね': 'ne', 'の': 'no',
+    'は': 'ha', 'ひ': 'hi', 'ふ': 'fu', 'へ': 'he', 'ほ': 'ho',
+    'ま': 'ma', 'み': 'mi', 'む': 'mu', 'め': 'me', 'も': 'mo',
+    'や': 'ya', 'ゆ': 'yu', 'よ': 'yo',
+    'ら': 'ra', 'り': 'ri', 'る': 'ru', 'れ': 're', 'ろ': 'ro',
+    'わ': 'wa', 'を': 'wo',
+    'ん': 'n',
+    'が': 'ga', 'ぎ': 'gi', 'ぐ': 'gu', 'げ': 'ge', 'ご': 'go',
+    'ざ': 'za', 'じ': 'ji', 'ず': 'zu', 'ぜ': 'ze', 'ぞ': 'zo',
+    'だ': 'da', 'ぢ': 'ji', 'づ': 'zu', 'で': 'de', 'ど': 'do',
+    'ば': 'ba', 'び': 'bi', 'ぶ': 'bu', 'べ': 'be', 'ぼ': 'bo',
+    'ぱ': 'pa', 'ぴ': 'pi', 'ぷ': 'pu', 'ぺ': 'pe', 'ぽ': 'po',
+    'きゃ': 'kya', 'きゅ': 'kyu', 'きょ': 'kyo',
+    'しゃ': 'sha', 'しゅ': 'shu', 'しょ': 'sho',
+    'ちゃ': 'cha', 'ちゅ': 'chu', 'ちょ': 'cho',
+    'にゃ': 'nya', 'にゅ': 'nyu', 'にょ': 'nyo',
+    'ひゃ': 'hya', 'ひゅ': 'hyu', 'ひょ': 'hyo',
+    'みゃ': 'mya', 'みゅ': 'myu', 'みょ': 'myo',
+    'りゃ': 'rya', 'りゅ': 'ryu', 'りょ': 'ryo',
+    'ぎゃ': 'gya', 'ぎゅ': 'gyu', 'ぎょ': 'gyo',
+    'じゃ': 'ja', 'じゅ': 'ju', 'じょ': 'jo',
+    'びゃ': 'bya', 'びゅ': 'byu', 'びょ': 'byo',
+    'ぴゃ': 'pya', 'ぴゅ': 'pyu', 'ぴょ': 'pyo'
+}
+
+glossary_katakana = {
+    'ア': 'a', 'イ': 'i', 'ウ': 'u', 'エ': 'e', 'オ': 'o',
+    'カ': 'ka', 'キ': 'ki', 'ク': 'ku', 'ケ': 'ke', 'コ': 'ko',
+    'サ': 'sa', 'シ': 'shi', 'ス': 'su', 'セ': 'se', 'ソ': 'so',
+    'タ': 'ta', 'チ': 'chi', 'ツ': 'tsu', 'テ': 'te', 'ト': 'to',
+    'ナ': 'na', 'ニ': 'ni', 'ヌ': 'nu', 'ネ': 'ne', 'ノ': 'no',
+    'ハ': 'ha', 'ヒ': 'hi', 'フ': 'fu', 'ヘ': 'he', 'ホ': 'ho',
+    'マ': 'ma', 'ミ': 'mi', 'ム': 'mu', 'メ': 'me', 'モ': 'mo',
+    'ヤ': 'ya', 'ユ': 'yu', 'ヨ': 'yo',
+    'ラ': 'ra', 'リ': 'ri', 'ル': 'ru', 'レ': 're', 'ロ': 'ro',
+    'ワ': 'wa', 'ヲ': 'wo', 'ン': 'n',
+    'ガ': 'ga', 'ギ': 'gi', 'グ': 'gu', 'ゲ': 'ge', 'ゴ': 'go',
+    'ザ': 'za', 'ジ': 'ji', 'ズ': 'zu', 'ゼ': 'ze', 'ゾ': 'zo',
+    'ダ': 'da', 'ヂ': 'ji', 'ヅ': 'zu', 'デ': 'de', 'ド': 'do',
+    'バ': 'ba', 'ビ': 'bi', 'ブ': 'bu', 'ベ': 'be', 'ボ': 'bo',
+    'パ': 'pa', 'ピ': 'pi', 'プ': 'pu', 'ペ': 'pe', 'ポ': 'po',
+    'キャ': 'kya', 'キュ': 'kyu', 'キョ': 'kyo',
+    'シャ': 'sha', 'シュ': 'shu', 'ショ': 'sho',
+    'チャ': 'cha', 'チュ': 'chu', 'チョ': 'cho',
+    'ニャ': 'nya', 'ニュ': 'nyu', 'ニョ': 'nyo',
+    'ヒャ': 'hya', 'ヒュ': 'hyu', 'ヒョ': 'hyo',
+    'ミャ': 'mya', 'ミュ': 'myu', 'ミョ': 'myo',
+    'リャ': 'rya', 'リュ': 'ryu', 'リョ': 'ryo',
+    'ギャ': 'gya', 'ギュ': 'gyu', 'ギョ': 'gyo',
+    'ジャ': 'ja', 'ジュ': 'ju', 'ジョ': 'jo',
+    'ビャ': 'bya', 'ビュ': 'byu', 'ビョ': 'byo',
+    'ピャ': 'pya', 'ピュ': 'pyu', 'ピョ': 'pyo',
+    'ヴァ': 'va', 'ヴィ': 'vi', 'ヴ': 'vu', 'ヴェ': 've', 'ヴォ': 'vo',
+    'ツァ': 'tsa', 'ツィ': 'tsi', 'ツェ': 'tse', 'ツォ': 'tso',
+    'ファ': 'fa', 'フィ': 'fi', 'フ': 'fu', 'フェ': 'fe', 'フォ': 'fo',
+    'イェ': 'ye',
+    'ウィ': 'wi', 'ウェ': 'we', 'ウォ': 'wo',
+    'シェ': 'she', 'ジェ': 'je',
+    'チェ': 'che',
+    'ティ': 'ti', 'ディ': 'di',
+    'トゥ': 'tu', 'ドゥ': 'du',
+    'クァ': 'kwa', 'クィ': 'kwi', 'クゥ': 'kwu', 'クェ': 'kwe', 'クォ': 'kwo',
+    'グァ': 'gwa', 'グィ': 'gwi', 'グゥ': 'gwu', 'グェ': 'gwe', 'グォ': 'gwo'
+}
+
+clear()
+
+correct_answer = 0
+false_answer = 0
+false_answer_list = {}
+items = {}
+number_of_char_start = 0
+number_of_char_end = 0
+
+while items == {}:
+    print("Type of quiz")
+    print("[1] Hiragana")
+    print("[2] Katakana")
+    print("")
+    quiz_type = input('')
+    if quiz_type == '1':
+        items = glossary_hiragana
+    elif quiz_type == '2':
+        items = glossary_katakana
+    else:
+        print('Invalid type')
+
+
+while True:
+    clear()
+
+    print("===", quiz_type.capitalize(), "===")
+    print("Incorrect Glossary: " + str(false_answer_list))
+    print("Correct Answers: " + str(correct_answer))
+    print("Incorrect Answers: " + str(false_answer))
+    print("")
+    
+    number_of_char = random.randint(1, 1)
+    random_word = ""
+    real_answer = ""
+    for i in range(number_of_char):
+        word = random.choice(list(items.keys()))
+        random_word += word
+        real_answer += items[word]
+
+    print(random_word)
+    answer = input("")
+    if answer == real_answer:
+        print("Correct! ✅")
+        correct_answer += 1
+    else:
+        print("Incorrect! ❌")
+        print("Correct Answer is : " + real_answer)
+        false_answer_list[random_word] = real_answer
+        false_answer += 1
+
+    time.sleep(0.5)
+    clear()
--- kanji.py
+++ kanji.py
@@ -2,101 +2,107 @@ import os
 import random
 import time
 
-clear = lambda: os.system('cls' if os.name == 'nt' else 'clear')
+
+def clear(): return os.system('cls' if os.name == 'nt' else 'clear')
+
 
 radicals = {
-  # 17 Mar : 10 Radicals
-  "一": "ground", "亅": "barb", "ハ": "fins", "丶": "drop", "丿": "slide",
-  "二": "two", "亠": "lid", "大": "big", "人": "person", "入": "enter",
-  # 18 Mar : 5 Radicals
-  '力': 'power', '勹': 'prison', '口': 'mouth', '女': ['woman', 'women'], '七': 'seven',
+    # 17 Mar : 10 Radicals
+    "一": "ground", "亅": "barb", "ハ": "fins", "丶": "drop", "丿": "slide",
+    "二": "two", "亠": "lid", "大": "big", "人": "person", "入": "enter",
+    # 18 Mar : 5 Radicals
+    '力': 'power', '勹': 'prison', '口': 'mouth', '女': ['woman', 'women'], '七': 'seven',
 }
 
 kanji_to_meaning = {
-  # 17 Mar
-  '人': ['person', 'people'], '太': 'fat', '大': 'big', '六': 'six', '一': 'one', 
-  '二': 'two', '三': 'three', '了': 'finish', '久': 'long time', '入': 'enter',
-  '才': 'talent', '八': 'eight',
-  # 18 Mar
-  '力': 'power', '口': 'mouth', '人口': 'population', '女': 'woman', '七': 'seven',
+    # 17 Mar
+    '人': ['person', 'people'], '太': 'fat', '大': 'big', '六': 'six', '一': 'one',
+    '二': 'two', '三': 'three', '了': 'finish', '久': 'long time', '入': 'enter',
+    '才': 'talent', '八': 'eight',
+    # 18 Mar
+    '力': 'power', '口': 'mouth', '人口': 'population', '女': 'woman', '七': 'seven',
 }
 
 kanji_to_hiragana = {
-  # 17 Mar : 12 Kanji
-  '人': ['じん', 'にん', 'ひと'], '太': 'たい', '大': ['だい', 'たい'], '六': 'ろく', '一': 'いち', 
-  '二': 'に', '三': 'さん', '了': 'りょう', '久': ['きゅう', 'く', 'ひさ'], '入': 'にゅう',
-  '才': 'さい', '八': 'はち',
-  # 18 Mar : 5 Kanji
-  '力': ['ちから', 'りょく', 'りき'], '口': ['こう', 'く', 'くち'], '人口': ['じんこう'], '女': ['じょ', 'おんな'], '七': ['なな', 'しち'],
+    # 17 Mar : 12 Kanji
+    '人': ['じん', 'にん', 'ひと'], '太': 'たい', '大': ['だい', 'たい'], '六': 'ろく', '一': 'いち',
+    '二': 'に', '三': 'さん', '了': 'りょう', '久': ['きゅう', 'く', 'ひさ'], '入': 'にゅう',
+    '才': 'さい', '八': 'はち',
+    # 18 Mar : 5 Kanji
+    '力': ['ちから', 'りょく', 'りき'], '口': ['こう', 'く', 'くち'], '人口': ['じんこう'], '女': ['じょ', 'おんな'], '七': ['なな', 'しち'],
 }
 
 false_answer = 0
 false_answer_list = {}
 correct_answer = 0
-
 items = {}
 
+
 clear()
-  
+
 while items == {}:
-  print("Type of quiz")
-  print("[1] Radicals")
-  print("[2] Kanji to English")
-  print("[3] Kanji To Hiragana")
-  print("")
-  quiz_type = input('')
-  if quiz_type == '1': items = radicals
-  elif quiz_type == '2': items = kanji_to_meaning
-  elif quiz_type == '3': items = kanji_to_hiragana
-  else: print('Invalid type')
+    print("Type of quiz")
+    print("[1] Radicals")
+    print("[2] Kanji to English")
+    print("[3] Kanji To Hiragana")
+    print("")
+    quiz_type = input('')
+    if quiz_type == '1':
+        items = radicals
+    elif quiz_type == '2':
+        items = kanji_to_meaning
+    elif quiz_type == '3':
+        items = kanji_to_hiragana
+    else:
+        print('Invalid type')
 
 
 while True:
-  clear()
-
-  if items == {}:
-    print('Finished! 🎉')
-    print('')
-    print('== Result ==')
-    print('')
-    print(f'Correct: {correct_answer}')
-    print(f'Incorrect: {false_answer}')
-    print('')
-    print(f'Need to study more:')
-    print(false_answer_list)
-    print('')
-    input('Press Enter to exit...')
     clear()
-    break
-  
-  print("===", quiz_type.capitalize(), "===")
-  print("")
-  print(f'Incorrect List: {false_answer_list}')
-  print(f'Correct: {correct_answer}, Incorrect: {false_answer}')
-  print("")
-  
-  item, real_answer = random.choice(list(items.items()))
-  print(item)
-  print("")
-  user_answer = input()
-  
-  print("")
-  if type(real_answer) == list and user_answer in real_answer:
-    correct_answer += 1
-    print('Correct ✅')
-  
-    # Delete the items if already correct
-    del items[item]
-  elif user_answer == real_answer:
-    correct_answer += 1
-    print('Correct ✅')
-    
-    # Delete the items if already correct
-    del items[item]
-  else:
-    false_answer += 1
-    false_answer_list[item] = real_answer
-    print('Incorrect ❌')
-    print(f'The correct answer is : {real_answer}')
-  time.sleep(1)
-  # input('Press Enter to continue...')
+
+    if items == {}:
+        print('Finished! 🎉')
+        print('')
+        print('== Result ==')
+        print('')
+        print(f'Correct: {correct_answer}')
+        print(f'Incorrect: {false_answer}')
+        print('')
+        print(f'Need to study more:')
+        print(false_answer_list)
+        print('')
+        input('Press Enter to exit...')
+        clear()
+        break
+
+    print("===", quiz_type.capitalize(), "===")
+    print("")
+    print(f'Incorrect List: {false_answer_list}')
+    print(f'Correct: {correct_answer}, Incorrect: {false_answer}')
+    print("")
+
+    item, real_answer = random.choice(list(items.items()))
+    print(item)
+    print("")
+    user_answer = input()
+
+    print("")
+    if type(real_answer) == list and user_answer in real_answer:
+        correct_answer += 1
+        print('Correct ✅')
+
+        # Delete the items if already correct
+        del items[item]
+    elif user_answer == real_answer:
+        correct_answer += 1
+        print('Correct ✅')
+
+        # Delete the items if already correct
+        del items[item]
+    else:
+        false_answer += 1
+        false_answer_list[item] = real_answer
+        print('Incorrect ❌')
+        print(f'The correct answer is : {real_answer}')
+    time.sleep(1)
+    # input('Press Enter to continue...')
